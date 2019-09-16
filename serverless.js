4305d23f4799500c24b9dc5bab6ced3608c6a36f
Eetu Tuomala <eetu.tuomala@maas.global>
2019-09-17 00:55:55 +0300

    move datasources and utils to separate file

--- serverless.js
+++ serverless.js
@@ -1,11 +1,12 @@
-const { Component, utils } = require('@serverless/core')
-const { mergeDeepRight, pick } = require('ramda')
+const { Component } = require('@serverless/core')
+const { mergeDeepRight, pick, map } = require('ramda')
 
 const {
   getClients,
   createOrUpdateGraphqlApi,
   createOrUpdateDataSources,
-  setupApiKey
+  removeChangedDataSources
+  // setupApiKey
 } = require('./utils')
 
 const defaults = {
@@ -21,13 +22,21 @@ class AwsAppSync extends Component {
     config.apiId = graphqlApi.apiId
     config.arn = graphqlApi.arn
     await createOrUpdateDataSources(appSync, config, this.context.debug)
+    await removeChangedDataSources(appSync, config, this.state, this.context.debug)
+
     // await setupApiKey(appSync, config, this.context.debug)
     this.state = pick(['apiId', 'arn'], config)
+    this.state.dataSources = map(pick(['name', 'type']), config.dataSources)
     await this.save()
     return { graphqlApi: pick(['apiId', 'arn'], config) }
   }
 
+  // eslint-disable-next-line no-unused-vars
   async remove(inputs = {}) {}
 }
 
 module.exports = AwsAppSync
+
+// create service role if not set
+// create datasources
+//
--- utils.js
+++ utils.js
@@ -1,23 +1,7 @@
-const { utils } = require('@serverless/core')
 const AWS = require('aws-sdk')
-const {
-  equals,
-  flatten,
-  filter,
-  concat,
-  clone,
-  find,
-  includes,
-  isEmpty,
-  isNil,
-  merge,
-  not,
-  pick,
-  pickBy,
-  map,
-  forEach,
-  pipe
-} = require('ramda')
+const { equals, find, isNil, merge, not, pick } = require('ramda')
+
+const { listAll } = require('./lib/utils')
 
 /**
  * Get AWS clients
@@ -32,25 +16,13 @@ const getClients = (credentials, region = 'us-east-1') => {
   }
 }
 
-/**
- * List all from appSync
- * @param {*} service
- * @param {*} command
- * @param {*} params
- */
-const listAll = async (service, command, params, key) => {
-  let result = []
-  let nextToken
-  do {
-    const currentParams = clone(params)
-    if (not(isNil(nextToken))) {
-      currentParams.nextToken = nextToken
-    }
-    const response = await service[command](params).promise()
-    result = concat(result, response[key])
-    nextToken = response.nextToken
-  } while (not(isNil(nextToken)))
-  return result
+const authentication = (authenticationType) => {
+  switch (authenticationType) {
+    case 'AMAZON_COGNITO_USER_POOLS':
+      return 'userPoolConfig'
+    case 'OPENID_CONNECT':
+      return 'openIDConnectConfig'
+  }
 }
 
 /**
@@ -60,13 +32,14 @@ const listAll = async (service, command, params, key) => {
  * @returns {object} - graphqlApi
  */
 const createOrUpdateGraphqlApi = async (appSync, config, debug) => {
-  const inputFields = ['name', 'authenticationType']
+  const inputFields = ['name', 'authenticationType', authentication(config.authenticationType)]
   const inputs = pick(inputFields, config)
   let graphqlApi
   if (config.apiId) {
     debug(`Fetching graphql API by API id '${config.apiId}'`)
     try {
       const response = await appSync.getGraphqlApi({ apiId: config.apiId }).promise()
+      // eslint-disable-next-line prefer-destructuring
       graphqlApi = response.graphqlApi
     } catch (error) {
       if (not(equals('NotFoundException', error.code))) {
@@ -87,163 +60,44 @@ const createOrUpdateGraphqlApi = async (appSync, config, debug) => {
   if (isNil(graphqlApi)) {
     debug(`Create a new graphql API`)
     const response = await appSync.createGraphqlApi(inputs).promise()
+    // eslint-disable-next-line prefer-destructuring
     graphqlApi = response.graphqlApi
   } else if (not(equals(inputs, pick(inputFields, graphqlApi)))) {
     debug(`Update an existing graphql API`)
     const response = await appSync
       .updateGraphqlApi(merge(inputs, { apiId: config.apiId }))
       .promise()
+    // eslint-disable-next-line prefer-destructuring
     graphqlApi = response.graphqlApi
   }
 
   return graphqlApi
 }
 
-const setupApiKey = async (appSync, config, debug) => {
-  if (not(isEmpty(config.apiKeys))) {
-    debug(`Fetching api keys`)
-    let nextToken
-    let apiKeys = []
-    // do {
-    //   let params = {
-    //     apiId: config.apiId
-    //   }
-    //   if (not(isNil(nextToken))) {
-    //     params.nextToken = nextToken
-    //   }
-    //   const response = await appSync.listApiKeys(params).promise()
-    //   nextToken = response.nextToken
-    // } while (not(isNil(nextToken)))
-
-    const deployedApiKeys = await listAll(
-      appSync,
-      'listApiKeys',
-      { apiId: config.apiId },
-      'apiKeys' // ?
-    )
-
-    await Promise.all(
-      map(async (apiKey) => {
-        console.log(apiKey)
-      }, config.apiKeys)
-    )
-
-    const isoDateRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/
-  }
-}
-
-const equalsByKeys = (keys, objA, objB) => equals(pick(keys, objA), pick(keys, objB))
-const equalsByKeysExcluded = (keys, objA, objB) =>
-  equals(
-    pickBy((val, key) => not(includes(key, keys)), objA),
-    pickBy((val, key) => not(includes(key, keys)), objB)
-  )
-
-const formatDataSource = (dataSource, region) => {
-  let result = {
-    name: dataSource.name,
-    type: dataSource.type
-  }
-  const config = clone(dataSource.config)
-  delete config.region
-  result.description = dataSource.description || null
-  switch (dataSource.type) {
-    case 'AWS_LAMBDA':
-      result = merge(result, {
-        lambdaConfig: config
-      })
-      break
-    case 'AMAZON_DYNAMODB':
-      result = merge(result, {
-        dynamodbConfig: config
-      })
-      result.dynamodbConfig.awsRegion = region
-      result.dynamodbConfig.useCallerCredentials = config.useCallerCredentials || false
-      break
-    case 'AMAZON_ELASTICSEARCH':
-      result = merge(result, {
-        elasticsearchConfig: config
-      })
-      result.dynamodbConfig.awsRegion = region
-      break
-    default:
-      break
-  }
-
-  return result
-}
-
-// {
-//   apiId: 'STRING_VALUE', /* required */
-//   name: 'STRING_VALUE', /* required */
-//   type: AWS_LAMBDA | AMAZON_DYNAMODB | AMAZON_ELASTICSEARCH | NONE | HTTP | RELATIONAL_DATABASE, /* required */
-//   description: 'STRING_VALUE',
-//   dynamodbConfig: {
-//     awsRegion: 'STRING_VALUE', /* required */
-//     tableName: 'STRING_VALUE', /* required */
-//     useCallerCredentials: true || false
-//   },
-//   elasticsearchConfig: {
-//     awsRegion: 'STRING_VALUE', /* required */
-//     endpoint: 'STRING_VALUE' /* required */
-//   },
-//   httpConfig: {
-//     authorizationConfig: {
-//       authorizationType: AWS_IAM, /* required */
-//       awsIamConfig: {
-//         signingRegion: 'STRING_VALUE',
-//         signingServiceName: 'STRING_VALUE'
-//       }
-//     },
-//     endpoint: 'STRING_VALUE'
-//   },
-//   lambdaConfig: {
-//     lambdaFunctionArn: 'STRING_VALUE' /* required */
-//   },
-//   relationalDatabaseConfig: {
-//     rdsHttpEndpointConfig: {
-//       awsRegion: 'STRING_VALUE',
-//       awsSecretStoreArn: 'STRING_VALUE',
-//       databaseName: 'STRING_VALUE',
-//       dbClusterIdentifier: 'STRING_VALUE',
-//       schema: 'STRING_VALUE'
-//     },
-//     relationalDatabaseSourceType: RDS_HTTP_ENDPOINT
-//   },
-//   serviceRoleArn: 'STRING_VALUE'
-
-const createOrUpdateDataSources = async (appSync, config, debug) => {
-  const deployedDataSources = await listAll(
-    appSync,
-    'listDataSources',
-    { apiId: config.apiId },
-    'dataSources'
-  )
-
-  const dataSourcesToDeploy = pipe(
-    map((dataSource) =>
-      merge(formatDataSource(dataSource, dataSource.config.region || config.region), {
-        apiId: config.apiId,
-        serviceRoleArn: dataSource.serviceRoleArn
-      })
-    ),
-    filter((dataSource) => {
-      const deployedDataSource = find(
-        (deployedDataSource) => equals(deployedDataSource.name, dataSource.name),
-        deployedDataSources
-      )
-      return not(equalsByKeysExcluded(['dataSourceArn', 'apiId'], deployedDataSource, dataSource))
-    })
-  )(config.dataSources)
-
-  await Promise.all(
-    map(async (dataSource) => appSync.createDataSource(dataSource).promise(), dataSourcesToDeploy)
-  )
-}
+// const setupApiKey = async (appSync, config, debug) => {
+//   TODO - how to index api keys e.g. name or id
+//   if (not(isEmpty(config.apiKeys))) {
+//     debug(`Fetching api keys`)
+//     let nextToken
+//     const apiKeys = []
+//     const deployedApiKeys = await listAll(
+//       appSync,
+//       'listApiKeys',
+//       { apiId: config.apiId },
+//       'apiKeys' // ?
+//     )
+//     await Promise.all(
+//       map(async (apiKey) => {
+//         console.log(apiKey)
+//       }, config.apiKeys)
+//     )
+//     const isoDateRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/
+//   }
+// }
 
 module.exports = {
   getClients,
   createOrUpdateGraphqlApi,
-  createOrUpdateDataSources,
-  setupApiKey
+  ...require('./lib/datasources')
+  // setupApiKey
 }
