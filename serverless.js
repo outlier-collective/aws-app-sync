7e9c8e496ac46c073177d8f5b61d7cb31334f7d7
Eetu Tuomala <eetu.tuomala@maas.global>
2019-09-18 00:32:09 +0300

    add functions create / update

--- serverless.js
+++ serverless.js
@@ -3,14 +3,15 @@ const { mergeDeepRight, pick, map } = require('ramda')
 
 const {
   getClients,
+  createSchema,
   createOrUpdateGraphqlApi,
   createOrUpdateDataSources,
-  removeObsoleteDataSources,
-  createSchema,
-  deleteGraphqlApi,
   createOrUpdateResolvers,
+  createOrUpdateFunctions,
+  removeObsoleteDataSources,
   removeObsoleteResolvers,
-
+  removeObsoleteFunctions,
+  deleteGraphqlApi
   // setupApiKey
 } = require('./utils')
 
@@ -29,13 +30,17 @@ class AwsAppSync extends Component {
     await createOrUpdateDataSources(appSync, config, this.context.debug)
     await createSchema(appSync, config, this.context.debug)
     await createOrUpdateResolvers(appSync, config, this.context.debug)
+    await createOrUpdateFunctions(appSync, config, this.context.debug)
+    // await setupApiKey(appSync, config, this.context.debug)
+
     await removeObsoleteDataSources(appSync, config, this.state, this.context.debug)
     await removeObsoleteResolvers(appSync, config, this.state, this.context.debug)
+    await removeObsoleteFunctions(appSync, config, this.state, this.context.debug)
 
-    // await setupApiKey(appSync, config, this.context.debug)
     this.state = pick(['apiId', 'arn'], config)
     this.state.dataSources = map(pick(['name', 'type']), config.dataSources)
     this.state.mappingTemplates = map(pick(['type', 'field']), config.mappingTemplates)
+    this.state.functions = map(pick(['name', 'dataSource']), config.functions)
     await this.save()
     return { graphqlApi: pick(['apiId', 'arn'], config) }
   }
